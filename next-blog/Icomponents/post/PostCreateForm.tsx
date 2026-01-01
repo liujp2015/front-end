35b7d16f0ded49a7b4be3fd82845a207221ce693
liujunpeng <392303104@qq.com>
2026-01-01 16:37:25 +0800

    updating style of creating page

--- next-blog/Icomponents/post/PostCreateForm.tsx
+++ next-blog/Icomponents/post/PostCreateForm.tsx
@@ -3,6 +3,8 @@ import { FC, useCallback, useRef, useState } from "react";
 import { PostCreateFormRef } from "./types";
 import { PostActionForm } from "./action-form";
 import { Save } from "lucide-react";
+import BackButton from "../home/BackButton";
+import { Button } from "@/components/ui/button";
 
 export const PostCreateForm: FC = () => {
   const ref = useRef<PostCreateFormRef>(null);
@@ -11,21 +13,24 @@ export const PostCreateForm: FC = () => {
   const changePedding = useCallback((value: boolean) => setPedding(value), []);
 
   return (
-    <div>
-      <button
-        disabled={pedding}
-        onClick={useCallback(async (e) => {
-          setPedding(true);
-          if (ref.current && typeof ref.current.create === "function") {
-            await ref.current.create(e);
-          }
-          setPedding(false);
-        }, [])}
-      >
-        {pedding ? "保存中" : "保存"}
-        <Save />
-      </button>
-      <PostActionForm ref={ref} type="create" setPedding={changePedding} />;
-    </div>
+    <>
+      <div className="flex justify-between">
+        <BackButton></BackButton>
+        <Button
+          disabled={pedding}
+          onClick={useCallback(async (e) => {
+            setPedding(true);
+            if (ref.current && typeof ref.current.create === "function") {
+              await ref.current.create(e);
+            }
+            setPedding(false);
+          }, [])}
+        >
+          {pedding ? "保存中" : "保存"}
+          <Save />
+        </Button>
+      </div>
+      <PostActionForm ref={ref} type="create" setPedding={changePedding} />
+    </>
   );
 };
--- next-blog/app/posts/create/page.tsx
+++ next-blog/app/posts/create/page.tsx
@@ -2,7 +2,13 @@ import { PostCreateForm } from "@/Icomponents/post/PostCreateForm";
 import React, { FC } from "react";
 
 const Page: FC = () => {
-  return <PostCreateForm></PostCreateForm>;
+  return (
+    <div className="flex flex-col flex-auto mt-20 items-center">
+      <div className="flex flex-auto flex-col space-y-6  bg-white/90 min-w-full lg:min-w-[60%] px-5 py-3 rounded-md  max-w-[90%] md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] flex-auto mb-5">
+        <PostCreateForm />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
