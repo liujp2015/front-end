d0f70522be11be1926f1f2ee042fabd0d72d3856
liujunpeng <392303104@qq.com>
2026-01-07 17:36:27 +0800

    feat:adding logo component

---
+++ next-blog/Icomponents/header/HeaderLogo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function HeaderLogo() {
+  return (
+    <div className="flex flex-none max-w-28">
+      <Link href={"/"} className=" block">
+        <Image
+          src="/next.svg"
+          alt="logo"
+          sizes="100%"
+          width={1000}
+          height={500}
+          style={{ width: "100%", height: "100%" }}
+        ></Image>
+      </Link>
+    </div>
+  );
+}
--- next-blog/Icomponents/header/index.tsx
+++ next-blog/Icomponents/header/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import HeaderLogo from "./HeaderLogo";
 
 export default function Header() {
   return (
-    <>
-      <div className="fixed h-16 top-0 w-full z-40  mt-0 bg-white/40 transition-[background-color] ease-in-out duration-300 flex flex-auto flex-col items-center mt-0">
-        13
+    <header className="fixed h-16 top-0 w-full z-40  mt-0 bg-white/40 transition-[background-color] ease-in-out duration-300 flex flex-auto flex-col items-center">
+      <div className="max-w-[90%] md:max-w-[80%] lg:max-w-[70%] xl:max-w-[60%] flex-auto mb-5 flex h-14 w-full justify-between flex-row mb-0 transition-[height] ease-in-out duration-300 ">
+        <HeaderLogo />
       </div>
-    </>
+    </header>
   );
 }
